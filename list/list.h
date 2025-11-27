73ad829d9afe440d40f11332422c1a8ef3e15be0
shizhuo <m5201314202201@163.com>
2025-11-27 18:32:02 +0800

    rebbuild

--- list/list.h
+++ list/list.h
@@ -32,5 +32,35 @@ public:
     //在链表末尾添加元素
     void push_back(const T& value);
     //在链表开头添加元素
-    void push_front(const T& value);    
+    void push_front(const T& value);   
+    //获取链表中节点的数量
+    size_t getSize()const;
+    //访问链表中的元素
+    T& operator[](size_t index);
+    //const版本访问链表中的元素
+    const T& operator[](size_t index)const;
+    //删除链表末尾的元素
+    void pop_back();
+    //删除链表开头的元素
+    void pop_front();
+    //获取指定值的节点
+    Node* getNode(const T& val);
+    //查找指定值的节点
+    T* find(const T& val);
+    //删除指定值的节点
+    void remove(const T& val);
+    //判断是否为空
+    bool empty();
+    //清空链表
+    void clear();
+    //使用迭代器遍历链表的开始位置
+    Node* begin();
+    //使用迭代器遍历链表的结束位置
+    Node* end();
+    //使用迭代器遍历链表的开始位置const版本
+    const Node* begin()const;
+    //使用迭代器遍历链表的结束位置const版本
+    const Node* end()const;
+    //打印链表中的元素
+    void printElements()const;
 }
